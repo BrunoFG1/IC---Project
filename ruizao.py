025d342efa30731940ef1a5ede121f264e0efabb
Rui Gil <119351777+ruipedrogil@users.noreply.github.com>
2025-11-03 13:58:03 +0000

    changed model type from sequential to model to handle multiple inputs
    Refactor neural network model to support multiple inputs and update normalization for observations.

--- ruizao.py
+++ ruizao.py
@@ -1,26 +1,57 @@
 import random
 import numpy as np
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, Flatten, Dense
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Conv2D, Flatten, Dense, Input, Concatenate
 
 # Rui -> Estratégia RN + CE (Redes Neuronais + Computação Evolucionária)
 
 VISION = 3
 OBS_SHAPE = (2 * VISION + 1, 2 * VISION + 1, 1)
 NUM_ACTIONS = 7
+STUN_TIME = 10.0
 
-def convNN():
-    model = Sequential([
-        Conv2D(8, (3, 3), activation='relu', input_shape=OBS_SHAPE),
-        Flatten(),
-        Dense(32, activation='relu'),
-        Dense(NUM_ACTIONS, activation='linear')
-    ])
+def modelNN():
+    
+    """Neural network with multiple inputs (in this case 3 inputs, for the obs, holding and stun)"""
+    
+    obs_input = Input(shape=OBS_SHAPE, name='obs_input')
+    x_vis = Conv2D(8, (3,3), activation='relu')(obs_input)
+    x_vis = Flatten()(x_vis) 
+    
+    hold_input = Input(shape=(1,), name='hold_input')
+    x_hold = Dense(4, activation='relu')(hold_input)
+    
+    stun_input = Input(shape=(1,), name='stun_input')
+    x_stun = Dense(4, activation='relu')(stun_input)
+
+    #Here we concat the 3 inputs in a single vector
+    concat = Concatenate()([x_vis, x_hold, x_stun])
+
+    hid_layer = Dense(16, activation='relu')(concat)
+    output = Dense(NUM_ACTIONS, activation='linear')(hid_layer)
+    model = Model(inputs=[obs_input, hold_input, stun_input], outputs=output)
     return model
 
+glob_model = modelNN()
+
+def set_model_weights(chromo):
+    global glob_model
+
+def get_model_chromo_shape():
+    global glob_model
+    tot_weights = 0
+    # each i is correspondent to the weights of each layer in the neural network
+    for i in glob_model.get_weights():
+        tot_weights += i.size
+    return tot_weights        
 
 def policy(obs, agent_id):
-    obs_array = np.array(obs) / 255.0  #normalizar os dados para a rede funcionar como deve ser
+    global glob_model
+
+    obs_array = np.array(obs) / 250.0  # data normalization
     return random.randint(0, 6)
+
+# def load_weights(chromo_weights, agent_idx):
+
 
 
