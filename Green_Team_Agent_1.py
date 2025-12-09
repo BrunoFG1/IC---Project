bba003b65a12fb203a07afa581460e37d3b02bce
Rui Gil <119351777+ruipedrogil@users.noreply.github.com>
2025-12-09 12:18:30 +0000

    Enxame aplicado
    Refactor agent logic to include A* pathfinding and PSO weights management.

--- Green_Team_Agent_1.py
+++ Green_Team_Agent_1.py
@@ -1,123 +1,187 @@
-import random
 import numpy as np
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Conv2D, Flatten, Dense, Input, Concatenate
+import heapq
 
-# Rui -> Estratégia RN + CE (Redes Neuronais + Computação Evolucionária) AGENTE 1
 
-VISION = 3
-OBS_SHAPE = (2 * VISION + 1, 2 * VISION + 1, 1)
-NUM_ACTIONS = 7
-STUN_TIME = 10.0
+class MyAgentState:
+    def __init__(self):
+        self.global_pos = (0, 0)
+        self.base_global_pos = None
+        self.visited = set()
+        self.last_action = 0
+        
+my_state = MyAgentState()
 
-def modelNN():
+# [Custo, Heurística, Repulsão]
+PSO_WEIGHTS = [2.35977368, 1.84191254, 0.01] 
+
+# PSO functions
+def get_model_particle_shape():
+    return 3
+
+def set_model_weights(weights):
+    global PSO_WEIGHTS
+    PSO_WEIGHTS = weights
+
+# A* algorithm
+def manhattan_dist(x, y):
+    return abs(x[0] - y[0]) + abs(x[1] - y[1])
+
+def path_rebuild(came_from, current):
+    path = [current]
+    while current in came_from:
+        current = came_from[current]
+        path.insert(0, current)
+    return path
+
+def a_star_pso(grid, start, end, teammate_pos, weights):
+    w_g, w_h, w_team = weights
+    rows, cols = grid.shape
     
-    """Neural network with multiple inputs (in this case 3 inputs, for the obs, holding and stun)"""
+    open_list = []
+    start_h = manhattan_dist(start, end) * w_h
+    heapq.heappush(open_list, (start_h, start))
     
-    obs_input = Input(shape=OBS_SHAPE, name='obs_input')
-    x_vis = Conv2D(8, (3,3), activation='relu')(obs_input)
-    x_vis = Flatten()(x_vis) 
+    came_from = {}
+    g_score = {start: 0}
     
-    hold_input = Input(shape=(1,), name='hold_input')
-    x_hold = Dense(4, activation='relu')(hold_input)
+    while open_list:
+        current = heapq.heappop(open_list)[1]
+
+        if current == end:
+            return path_rebuild(came_from, current)
+
+        neighbors = [
+            (current[0]-1, current[1]), (current[0]+1, current[1]),
+            (current[0], current[1]-1), (current[0], current[1]+1)
+        ]
+
+        for neighbor in neighbors:
+            ni, nj = neighbor
+            
+            # Limites e Paredes
+            if ni < 0 or ni >= rows or nj < 0 or nj >= cols: continue
+            if grid[ni, nj] == 50: continue
+
+
+            move_cost = 1
+            
+            # Repulsão do Amigo
+            if teammate_pos and manhattan_dist(neighbor, teammate_pos) <= 1:
+                move_cost += w_team 
+
+            tentative_g_score = g_score[current] + (move_cost * w_g)
+
+            if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = tentative_g_score
+                h = manhattan_dist(neighbor, end) * w_h
+                f = tentative_g_score + h
+                heapq.heappush(open_list, (f, neighbor))
+
+    return []
+
+
+def clamp_target_to_grid(local_target, rows, cols):
+    ty, tx = local_target
+    cy = max(0, min(ty, rows-1))
+    cx = max(0, min(tx, cols-1))
+    return (cy, cx)
+
+
+def policy(obs, agent_id=0):
+    global my_state, PSO_WEIGHTS
     
-    stun_input = Input(shape=(1,), name='stun_input')
-    x_stun = Dense(4, activation='relu')(stun_input)
-
-    #Here we concat the 3 inputs in a single vector
-    concat = Concatenate()([x_vis, x_hold, x_stun])
-
-    hid_layer = Dense(16, activation='relu')(concat)
-    output = Dense(NUM_ACTIONS, activation='linear')(hid_layer)
-    model = Model(inputs=[obs_input, hold_input, stun_input], outputs=output)
-    return model
-
-glob_model = modelNN()
-
-def set_model_weights(chromo):
-    """
-    For each weight layer in glob_model, its shape and size are discovered.
-    Then, a slice with that size is cut from the chromosome, it is reshaped 
-    to have that shape, and its used to replace the weights of that layer 
-    in the model.
+    grid = obs["grid"][:, :, 0]
+    vision = grid.shape[0] // 2
+    agent_center_pos = (vision, vision)
+    holding_flag = obs["holding"][0]
     
-    """
-    global glob_model
-    new_weights = []
-    cur_idx = 0
-    # each i is correspondent to the weights of each layer in the neural network
-    for i in glob_model.get_weights():
-        size = i.size
-        shape = i.shape
-        # we here get a slice from the chromossome and we give it the layer shape 
-        w_slice = chromo[cur_idx : cur_idx + size]
-        new_weights.append(w_slice.reshape(shape))
-        cur_idx += size
-    # in the end we have a list of arrays of each chromossome slice reshaped to match the model´s layer shape 
-    glob_model.set_weights(new_weights)
+    # refresh gps
+    move_map = {1: (-1, 0), 2: (1, 0), 3: (0, -1), 4: (0, 1), 0: (0, 0)}
+    if my_state.last_action in move_map:
+        dy, dx = move_map[my_state.last_action]
+        my_state.global_pos = (my_state.global_pos[0] + dy, my_state.global_pos[1] + dx)
+
+    # gps calibration
+    bases = np.argwhere(grid == 250)
+    if len(bases) > 0:
+        by, bx = bases[0]
+        rel_y, rel_x = by - vision, bx - vision
+        
+        # Posição absoluta desta base que estamos a ver
+        observed_base_global = (my_state.global_pos[0] + rel_y, my_state.global_pos[1] + rel_x)
+        
+        # Só atualizamos se for a primeira vez OU se estiver perto (<15) da nossa base original
+        if my_state.base_global_pos is None:
+            my_state.base_global_pos = observed_base_global
+        else:
+            dist = abs(my_state.base_global_pos[0] - observed_base_global[0]) + \
+                   abs(my_state.base_global_pos[1] - observed_base_global[1])
+            
+            if dist < 15: # Our base, recalibrate the GPS
+                my_state.base_global_pos = observed_base_global
+                print("MY BASE!!!!")
+            else: print("ENEMY BASE!!!")
+            # If dist > 15, then it is the enemy base, ignore.
+
+    my_state.visited.add(my_state.global_pos)
     
+    teammate_pos = None 
 
-def get_model_chromo_shape():
-    """
-    Here, we will get the size (length) of the chromossome's vector.
+    blockers = np.argwhere((grid != 0) & (grid != 50) & (grid != 100) & (grid != 250))
+    for b in blockers:
+        if tuple(b) != agent_center_pos:
+            teammate_pos = tuple(b) # Achámos um obstáculo móvel!
+            break
     
-    """
-    global glob_model
-    tot_weights = 0
-    # each i is correspondent to the weights of each layer in the neural network
-    for i in glob_model.get_weights():
-        tot_weights += i.size
-    return tot_weights        
-
-def policy(obs, agent_id):
-    """
-    The policy function will use the 'glob_model' which is the modelNN() function
-    and the dictionary 'obs' to decide the next action
-    
-    """
-    global glob_model
-
-    vis = np.array(obs["grid"]) / 250.0  # data normalization
-    vis = vis.reshape(1, OBS_SHAPE[0], OBS_SHAPE[1], 1)
-
-    hold = np.array(obs["holding"])
-    hold = hold.reshape(1, 1)
-
-    stun = np.array(obs["stun"]) / STUN_TIME
-    stun = stun.reshape(1, 1)
-
-    #  Action predict
-    act_pref = glob_model([vis, hold, stun], training=False).numpy()
-
-    # Action Choice
-    cho_act = np.argmax(act_pref[0])
-
-    return int(cho_act)
-
-def load_weights(chromo_weights, agent_idx):
-    """
-    Load the weights from the .npy file and 
-    that file is used in this agent
-    
-    """
-    try:
-        chromo = np.load(chromo_weights)
-        agent_len = get_model_chromo_shape()
-        # agent 1 will take the first half from the weights list
-        if agent_idx == 1:
-            c1 = chromo[0 : agent_len]
-            set_model_weights(c1)
-        # agent 2 will take the second half from the weights list
-        elif agent_idx == 2:
-            c2 = chromo[agent_len :]
-            set_model_weights(c2)
-        print(f"Agent {agent_idx} loaded the {chromo_weights} weights")
-    except FileNotFoundError:
-        print(f"File {chromo_weights} not found, currently using random weights")
-    except Exception as e:
-        print(f"Failed to load the weights: {e}, using random weights")
-
-load_weights("best_chrome_team.npy", 1)
+    # decision
+    target = None
+    if holding_flag:
+        # back to the base
+        if my_state.base_global_pos:
+            diff_y = my_state.base_global_pos[0] - my_state.global_pos[0]
+            diff_x = my_state.base_global_pos[1] - my_state.global_pos[1]
+            raw_target = (vision + diff_y, vision + diff_x)
+            target = clamp_target_to_grid(raw_target, grid.shape[0], grid.shape[1])
+        else:
+            target = (vision, vision-5)
+    else:
+        # go get the flag
+        flags = np.argwhere(grid == 100)
+        if len(flags) > 0:
+            target = tuple(flags[0])
 
 
+    action = 0
+    if target:
+        path = a_star_pso(grid, agent_center_pos, target, teammate_pos, PSO_WEIGHTS)
+        if len(path) > 1:
+            next_node = path[1]
+            dy, dx = next_node[0] - vision, next_node[1] - vision
+            if dy == -1: action = 1
+            elif dy == 1: action = 2
+            elif dx == -1: action = 3
+            elif dx == 1: action = 4
 
+    # exploration
+    if action == 0:
+        possible = [1, 2, 3, 4]
+        np.random.shuffle(possible)
+        for act in possible:
+            dy, dx = move_map[act]
+            if grid[vision+dy, vision+dx] == 50: continue
+            
+            future_global = (my_state.global_pos[0] + dy, my_state.global_pos[1] + dx)
+            if future_global not in my_state.visited:
+                action = act
+                break
+        
+        if action == 0:
+            for act in possible:
+                dy, dx = move_map[act]
+                if grid[vision+dy, vision+dx] != 50: 
+                    action = act
+                    break
+
+    my_state.last_action = action
+    return action
